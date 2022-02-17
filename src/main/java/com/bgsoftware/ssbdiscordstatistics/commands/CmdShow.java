671ed6741b9afa91b1723758c3f0d114e31827ef
OmerBenGera <omerthepro126@gmail.com>
2022-02-17 22:28:51 +0200

    Fixed getting roles by their names causing issues when not using default names

--- src/main/java/com/bgsoftware/ssbdiscordstatistics/commands/CmdShow.java
+++ src/main/java/com/bgsoftware/ssbdiscordstatistics/commands/CmdShow.java
@@ -9,11 +9,16 @@ import com.bgsoftware.superiorskyblock.api.wrappers.SuperiorPlayer;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.Member;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.TextChannel;
 
+import javax.annotation.Nullable;
+
 public final class CmdShow implements ICommand {
 
-    private static final PlayerRole ADMIN_ROLE = SuperiorSkyblockAPI.getPlayers().getPlayerRole("Admin");
-    private static final PlayerRole MOD_ROLE = SuperiorSkyblockAPI.getPlayers().getPlayerRole("Moderator");
-    private static final PlayerRole MEMBER_ROLE = SuperiorSkyblockAPI.getPlayers().getPlayerRole("Member");
+    @Nullable
+    private static final PlayerRole ADMIN_ROLE = SuperiorSkyblockAPI.getSuperiorSkyblock().getRoles().getLastRole().getPreviousRole();
+    @Nullable
+    private static final PlayerRole MOD_ROLE = ADMIN_ROLE == null ? null : ADMIN_ROLE.getPreviousRole();
+    @Nullable
+    private static final PlayerRole MEMBER_ROLE = SuperiorSkyblockAPI.getSuperiorSkyblock().getRoles().getDefaultRole();
 
     @Override
     public String getUsage() {
@@ -35,14 +40,14 @@ public final class CmdShow implements ICommand {
         SuperiorPlayer superiorPlayer = SuperiorSkyblockAPI.getPlayer(args[0]);
         Island island = superiorPlayer == null ? SuperiorSkyblockAPI.getGrid().getIsland(args[0]) : superiorPlayer.getIsland();
 
-        if(island == null){
+        if (island == null) {
             textChannel.sendMessage(EmbedBuilder.error()
                     .withField("Error", "Invalid island " + args[0], false)
                     .build()).queue();
             return;
         }
 
-        textChannel.sendMessage(EmbedBuilder.info()
+        EmbedBuilder embedBuilder = EmbedBuilder.info()
                 .withTitle("Island Information")
                 .withField(":man_frowning: Leader", island.getOwner().getName(), true)
                 .withField(":notepad_spiral: Name", island.getName(), true)
@@ -50,14 +55,25 @@ public final class CmdShow implements ICommand {
                 .withField(":bank: Bank", "$" + island.getIslandBank().getBalance(), true)
                 .withField(":dollar: Worth", "$" + island.getWorth(), true)
                 .withField(":coin: Level", island.getIslandLevel() + "", true)
-                .withBlankField(false)
-                .withField(":red_circle: Admins", getIslandMemberNames(island, ADMIN_ROLE), true)
-                .withField(":green_circle: Moderators", getIslandMemberNames(island, MOD_ROLE), true)
-                .withField(":blue_circle: Members", getIslandMemberNames(island, MEMBER_ROLE), true)
-                .build()).queue();
+                .withBlankField(false);
+
+        if (ADMIN_ROLE != null) {
+            embedBuilder.withField(":red_circle: Admins", getIslandMemberNames(island, ADMIN_ROLE), true);
+        }
+
+        if (MOD_ROLE != null) {
+            embedBuilder.withField(":green_circle: Moderators", getIslandMemberNames(island, MOD_ROLE), true);
+        }
+
+        if (MEMBER_ROLE != null) {
+            embedBuilder.withField(":blue_circle: Members", getIslandMemberNames(island, MEMBER_ROLE), true);
+        }
+
+
+        textChannel.sendMessage(embedBuilder.build()).queue();
     }
 
-    private static String getIslandMemberNames(Island island, PlayerRole playerRole){
+    private static String getIslandMemberNames(Island island, PlayerRole playerRole) {
         StringBuilder stringBuilder = new StringBuilder();
         island.getIslandMembers(false).stream().filter(superiorPlayer -> superiorPlayer.getPlayerRole() == playerRole)
                 .forEach(superiorPlayer -> stringBuilder.append("\n").append(superiorPlayer.getName()));
